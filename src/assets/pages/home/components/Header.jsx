10102c160c65b548466ba06aba081a87444d15ad
mahuena <ads23a00024y@ait.edu.gh>
2024-08-27 17:39:18 +0000

    working on the header

--- src/assets/pages/home/components/Header.jsx
+++ src/assets/pages/home/components/Header.jsx
@@ -16,21 +16,21 @@ export const Header = () => {
 
     return (
         <div
-            className="relative isolate overflow-hidden py-5 sm:py-10 min-h-[600px] md:min-h-[750px] lg:min-h-screen">
+            className="relative isolate overflow-hidden py-5 sm:py-10 min-h-[600px] md:min-h-[700px] lg:min-h-screen">
             <img src="src/assets/images/hero-bg.png" alt=""
                  className="absolute inset-0 -z-10 h-full w-full object-cover object-center md:object-right lg:object-center opacity-85"/>
 
-            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8 min-h-[600px] md:min-h-[750px] lg:min-h-[1000px]">
                 <div className="flex justify-between items-center">
                     <div>
-                        <span className="font-bold text-[24px] text-[#ffffff] uppercase">Logo</span>
+                        <span className="font-bold text-2xl lg:text-4xl text-[#ffffff] uppercase">Logo</span>
                     </div>
                     <div onClick={() => setOpen(!open)}
                          className='absolute right-8 top-6 cursor-pointer md:hidden w-7 h-7 text-white'>
                         {open ? <XMarkIcon/> : <Bars3BottomRightIcon/>}
                     </div>
 
-                    <ul className="hidden md:flex items-center justify-center space-x-8 mt-4">
+                    <ul className="hidden md:flex items-center justify-center space-x-10 mt-4">
                         {Links.map((link) => (
                             <li key={link.name} className='font-semibold'>
                                 <a href={link.link} className='text-white'>{link.name}</a>
@@ -45,34 +45,45 @@ export const Header = () => {
                         </motion.div>
                     </ul>
                 </div>
-            </div>
 
-            <motion.ul
-                initial={{height: 0}}
-                animate={{height: open ? 'auto' : 0}}
-                transition={{
-                    duration: 0.3,
-                    type: 'spring',
-                    stiffness: 150,
-                    damping: 20,
-                    ease: 'easeInOut'
-                }}
-                className={`md:hidden overflow-hidden transition-all duration-500 ease-in-out w-full p-2 mt-2`}>
-                {Links.map((link) => (
-                    <li key={link.name} className='font-semibold'>
-                        <a href={link.link} className='text-white'>{link.name}</a>
-                    </li>
-                ))}
-                <div onClick={() => navigate('/login')}>
-                    <span className="text-md text-white uppercase">Login</span>
+                <motion.ul
+                    initial={{height: 0}}
+                    animate={{height: open ? 'auto' : 0}}
+                    transition={{
+                        duration: 0.3,
+                        type: 'spring',
+                        stiffness: 150,
+                        damping: 20,
+                        ease: 'easeInOut'
+                    }}
+                    className={`md:hidden overflow-hidden transition-all duration-500 ease-in-out w-full p-2 mt-2`}>
+                    {Links.map((link) => (
+                        <li key={link.name} className='font-semibold'>
+                            <a href={link.link} className='text-white'>{link.name}</a>
+                        </li>
+                    ))}
+                    <div onClick={() => navigate('/login')}>
+                        <span className="text-md text-white uppercase">Login</span>
+                    </div>
+                </motion.ul>
+
+                <div
+                    className="text-start text-white mt-20 lg:mt-40 m-auto h-[300px] md:h-[500px] lg:h-[600px] items-center flex">
+                    <div className=" justify-start flex-col max-w-7xl">
+                        <h1 className="text-3xl lg:text-6xl font-bold uppercase tracking-wider space-y-10 mb-8 md:mb-14 lg:mb-20">We
+                            provide
+                            best <br/>
+                            healthcare</h1>
+                        <p className="mt-4 text-lg">We provide the best services to help you succeed.</p>
+                        <motion.button
+                            whileHover={{scale: 1.1}}
+                            className="border-2 bg-white rounded-lg text-[#62d2a2] py-[10px] px-[30px] mt-5"
+                            onClick={() => navigate('/login')}
+                        >
+                            <span className="text-md uppercase">Read More</span>
+                        </motion.button>
+                    </div>
                 </div>
-            </motion.ul>
-
-            <div className="hero-content text-center text-white mt-10">
-                <h1 className="text-5xl font-bold">Welcome to Our Website</h1>
-                <p className="mt-4 text-lg">We provide the best services to help you succeed.</p>
-                <a href="#" className="mt-6 inline-block bg-primary px-5 py-3 rounded-full text-white">Get
-                    Started</a>
             </div>
         </div>
     )
