5da9ba3a7668bff475775ad2b5be3183e3b21cb3
mahuena <ads23a00024y@ait.edu.gh>
2024-08-29 10:41:07 +0000

    working on the header

--- src/assets/pages/home/components/Header.jsx
+++ src/assets/pages/home/components/Header.jsx
@@ -8,19 +8,19 @@ export const Header = () => {
     const [open, setOpen] = useState(false);
 
     const Links = [
-        {name: "HOME", link: "/"},
-        {name: "SERVICE", link: "/service"},
-        {name: "ABOUT", link: "/about"},
-        {name: "CONTACT", link: "/contact"},
+        {name: "Home", link: "/"},
+        {name: "Services", link: "/services"},
+        {name: "About", link: "/about"},
+        {name: "Contact", link: "/contact"},
     ];
 
     return (
         <div
-            className="relative isolate overflow-hidden py-5 sm:py-10 min-h-[600px] md:min-h-[700px] lg:min-h-screen">
+            className="relative isolate overflow-hidden py-3">
             <img src="src/assets/images/hero-bg.png" alt=""
-                 className="absolute inset-0 -z-10 h-full w-full object-cover object-center md:object-right lg:object-center opacity-85"/>
+                 className="absolute inset-0 -z-10 w-full object-cover h-[600px] md:h-[700px] lg:h-screen object-center md:object-right lg:object-center opacity-85"/>
 
-            <div className="mx-auto max-w-7xl px-6 lg:px-8 min-h-[600px] md:min-h-[750px] lg:min-h-[1000px]">
+            <div className="mx-auto max-w-7xl px-4  md:px-10 min-h-[600px] md:min-h-[750px] lg:min-h-[1000px]">
                 <div className="flex justify-between items-center">
                     <div>
                         <span className="font-bold text-2xl lg:text-4xl text-[#ffffff] uppercase">Logo</span>
@@ -30,7 +30,7 @@ export const Header = () => {
                         {open ? <XMarkIcon/> : <Bars3BottomRightIcon/>}
                     </div>
 
-                    <ul className="hidden md:flex items-center justify-center space-x-10 mt-4">
+                    <ul className="hidden md:flex items-center justify-center gap-10 mt-4">
                         {Links.map((link) => (
                             <li key={link.name} className='font-semibold'>
                                 <a href={link.link} className='text-white'>{link.name}</a>
@@ -38,10 +38,10 @@ export const Header = () => {
                         ))}
                         <motion.div
                             whileHover={{scale: 1.1}}
-                            className="flex justify-center border-2 border-dashed rounded-lg border-white py-[10px] px-[30px]"
+                            className="flex justify-center border-2 border-dashed rounded-lg border-white cursor-pointer py-1.5 px-5"
                             onClick={() => navigate('/login')}
                         >
-                            <span className="text-md text-white uppercase">Login</span>
+                            <span className="text-sm md:text-md font-bold text-white">Login</span>
                         </motion.div>
                     </ul>
                 </div>
@@ -63,24 +63,27 @@ export const Header = () => {
                         </li>
                     ))}
                     <div onClick={() => navigate('/login')}>
-                        <span className="text-md text-white uppercase">Login</span>
+                        <span className="text-sm text-white">Login</span>
                     </div>
                 </motion.ul>
 
                 <div
-                    className="text-start text-white mt-20 lg:mt-40 m-auto h-[300px] md:h-[500px] lg:h-[600px] items-center flex">
-                    <div className=" justify-start flex-col max-w-7xl">
-                        <h1 className="text-3xl lg:text-6xl font-bold uppercase tracking-wider space-y-10 mb-8 md:mb-14 lg:mb-20">We
+                    className="text-start text-white mt-10 md:mt-0 m-auto h-[300px] md:h-[500px] lg:h-[600px] items-center flex">
+                    <div className=" justify-start max-w-2xl flex-col ">
+                        <h1 className="text-4xl md:text-5xl font-bold uppercase tracking-wider space-y-32 mb-8 md:mb-10">We
                             provide
                             best <br/>
                             healthcare</h1>
-                        <p className="mt-4 text-lg">We provide the best services to help you succeed.</p>
+                        <p className="my-4 text-sm md:text-md lg:text-lg">We provide the best services to help you
+                            succeed. Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic?
+                            Esse dicta aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto
+                            veritatis delectus repellat modi impedit sequi.</p>
                         <motion.button
                             whileHover={{scale: 1.1}}
-                            className="border-2 bg-white rounded-lg text-[#62d2a2] py-[10px] px-[30px] mt-5"
+                            className=" bg-white rounded-md text-[#62d2a2] py-1 px-4 md:py-1.5 md:px-5 mt-5"
                             onClick={() => navigate('/login')}
                         >
-                            <span className="text-md uppercase">Read More</span>
+                            <span className="text-sm md:text-lg">Read More</span>
                         </motion.button>
                     </div>
                 </div>
